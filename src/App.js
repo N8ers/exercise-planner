7c5d288b63065a1aaeee16758c5de2f0e1eb597f
Nathan <nathanraysheryak@gmail.com>
2021-05-12 07:43:20 -0400

    create basic poc to add exercises to a list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      newExercise: '',
+      exercises: ['pushup', 'situp', 'pullup']
+    };
+  }
+
+  updateNewExercise = (event) => {
+    event.preventDefault();
+    this.setState({
+      newExercise: event.target.value,
+    });
+  };
+
+  submitNewExercise = (event) => {
+    event.preventDefault();
+    this.setState({
+      exercises: [...this.state.exercises, this.state.newExercise],
+    });
+    this.setState({
+      newExercise: '',
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Exercise:</h1>
+        <ul>
+          {this.state.exercises.map((exercise, index) => (
+            <li key={exercise + index}>{exercise}</li>
+          ))}
+        </ul>
+
+        <form className="App" onSubmit={this.submitNewExercise}>
+          <input value={this.state.newExercise} onChange={this.updateNewExercise} />
+          <button>Submit</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
