9b91112aa1ce9a280898551388f6530a30c05fcd
Nathan <nathanraysheryak@gmail.com>
2021-05-28 06:43:34 -0400

    style nav bar w/ material ui

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,14 +1,23 @@
 import { Link } from "react-router-dom";
+import { AppBar, Toolbar, Button } from "@material-ui/core";
 
 function Nav() {
   return (
-    <header>
-      <nav>
-        <Link to="/">Home</Link>
-        <span> | </span>
-        <Link to="/new">New Exercise Plan</Link>
-      </nav>
-    </header>
+    <AppBar position="static">
+      <Toolbar>
+        <Button variant="contained" color="secondary" component={Link} to="/">
+          Home
+        </Button>
+        <Button
+          variant="contained"
+          color="secondary"
+          component={Link}
+          to="/new"
+        >
+          New Exercise Plan
+        </Button>
+      </Toolbar>
+    </AppBar>
   );
 }
 
